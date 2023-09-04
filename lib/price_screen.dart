43292cb5a79549563d20a8490b863c6093c19e34
Akshat-sGit <e21cseu0806@bennett.edu.in>
2023-09-04 13:09:24 +0530

    dropdown menu added

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,9 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +45,24 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: 'USD', 
+              items: [
+                DropdownMenuItem(
+                  child: Text('USD'),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text('EUR'),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text('INR'),
+                  value: 'INR',
+                ),
+              ],
+              onChanged: (value) => print(value),
+            )
           ),
         ],
       ),
